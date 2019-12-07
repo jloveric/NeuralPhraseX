5074b53ff7bbc8aafb4b1a6dd9815f6370900496
John Loverich <john.loverich@gmail.com>
2019-12-07 10:53:18 -0800

    First commit.  Not using any of the tensorflow stuff yet, just copying from the original phrasex.

---
+++ PhraseSequence.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class PhraseSequence {
+  constructor() {
+    this.sequence = [];
+  }
+
+  addToSequence(obj) {
+    this.sequence.push(obj);
+  }
+}
+
+module.exports = PhraseSequence;
